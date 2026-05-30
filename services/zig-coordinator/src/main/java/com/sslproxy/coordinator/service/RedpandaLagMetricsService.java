6d820adf48bdc94eb91f28d3eb6d77d93ac22de9
zlovtnik <racasantos@icloud.com>
2026-05-29 23:30:42 -0400

    fix: add @Autowired annotation to constructor for dependency injection
    The constructor was missing the @Autowired annotation, which prevented Spring from automatically injecting dependencies. Adding @Autowired enables proper wiring of MeterRegistry, CoordinatorProperties, etc. Also fixes missing trailing newline at end of file.

--- services/zig-coordinator/src/main/java/com/sslproxy/coordinator/service/RedpandaLagMetricsService.java
+++ services/zig-coordinator/src/main/java/com/sslproxy/coordinator/service/RedpandaLagMetricsService.java
@@ -14,6 +14,7 @@ import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -47,6 +48,7 @@ public class RedpandaLagMetricsService {
     private volatile AdminClient adminClient;
     private volatile boolean disabledLogged = false;
 
+    @Autowired
     public RedpandaLagMetricsService(MeterRegistry registry, CoordinatorProperties props) {
         this(registry, props, () -> AdminClient.create(adminClientConfig(props)), Clock.systemUTC());
     }
