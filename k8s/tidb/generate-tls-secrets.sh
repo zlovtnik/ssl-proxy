d7d3c8ba0fe0c6b4535fd469e86007a1ba846111
zlovtnik <racasantos@icloud.com>
2026-07-22 10:54:54 -0400

    feat: add TLS support for TiDB server and update related secrets

--- k8s/tidb/generate-tls-secrets.sh
+++ k8s/tidb/generate-tls-secrets.sh
@@ -10,10 +10,48 @@ openssl genrsa -out ${CERT_DIR}/ca.key 2048
 openssl req -new -x509 -days 3650 -key ${CERT_DIR}/ca.key -out ${CERT_DIR}/ca.crt \
   -subj "/CN=TiDB Client CA"
 
-echo "Creating Kubernetes secret..."
+echo "Creating client CA secret..."
 kubectl create secret generic ${SECRET_NAME} \
   --namespace=${NAMESPACE} \
   --from-file=ca.crt=${CERT_DIR}/ca.crt \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+echo "Generating TiDB server certificate signed by client CA..."
+cat > ${CERT_DIR}/tidb-server.cnf << 'TLS_EOF'
+[req]
+distinguished_name = req_distinguished_name
+req_extensions = req_ext
+prompt = no
+
+[req_distinguished_name]
+CN = ssl-proxy-tidb
+
+[req_ext]
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = ssl-proxy-tidb.ssl-proxy.svc.cluster.local
+DNS.2 = ssl-proxy-tidb
+TLS_EOF
+
+openssl req -newkey rsa:2048 -nodes \
+  -keyout ${CERT_DIR}/tidb-server.key \
+  -out ${CERT_DIR}/tidb-server.csr \
+  -config ${CERT_DIR}/tidb-server.cnf
+
+openssl x509 -req -days 3650 \
+  -in ${CERT_DIR}/tidb-server.csr \
+  -CA ${CERT_DIR}/ca.crt -CAkey ${CERT_DIR}/ca.key \
+  -CAcreateserial \
+  -out ${CERT_DIR}/tidb-server.crt \
+  -extfile ${CERT_DIR}/tidb-server.cnf \
+  -extensions req_ext
+
+echo "Creating TiDB server TLS secret..."
+kubectl create secret tls tidb-server-tls \
+  --namespace=${NAMESPACE} \
+  --cert=${CERT_DIR}/tidb-server.crt \
+  --key=${CERT_DIR}/tidb-server.key \
   --dry-run=client -o yaml | kubectl apply -f -
 
 echo "Generating account passwords..."
